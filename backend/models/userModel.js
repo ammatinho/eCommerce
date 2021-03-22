c7754442e4004df1f14ddf00f15da03a3c97ee76
ammatinho <anamargaridamatinho@gmail.com>
2021-03-22 10:55:53 +0100

    User Authentication Endpoint

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose'
+import bycrypt from 'bcryptjs'
 
 const userSchema = mongoose.Schema(
   {
@@ -25,6 +26,10 @@ const userSchema = mongoose.Schema(
     timestamps: true,
   }
 )
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bycrypt.compare(enteredPassword, this.password)
+}
 
 const User = mongoose.model('User', userSchema)
 
