09847b48d0e204340d18073a0b5392af29c60e3e
ammatinho <anamargaridamatinho@gmail.com>
2021-03-22 16:37:50 +0100

    User Registration & Password Encryption

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -31,6 +31,15 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bycrypt.compare(enteredPassword, this.password)
 }
 
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next()
+  }
+
+  const salt = await bycrypt.genSalt(10)
+  this.password = await bycrypt.hash(this.password, salt)
+})
+
 const User = mongoose.model('User', userSchema)
 
 export default User
