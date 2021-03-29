0d5dd7881a1438342d5af68a3a32fb7d8cd10bcc
ammatinho <anamargaridamatinho@gmail.com>
2021-03-29 13:59:33 +0200

    Navbar Name Bug

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -11,6 +11,7 @@ import {
   USER_REGISTER_SUCCESS,
   USER_UPDATE_PROFILE_FAIL,
   USER_UPDATE_PROFILE_REQUEST,
+  USER_UPDATE_PROFILE_RESET,
   USER_UPDATE_PROFILE_SUCCESS,
 } from '../constants/userConstants'
 
@@ -97,6 +98,8 @@ export const userUpdateProfileReducer = (state = {}, action) => {
         loading: false,
         error: action.payload,
       }
+    case USER_UPDATE_PROFILE_RESET:
+      return {}
     default:
       return state
   }
