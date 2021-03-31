4d6ead9e64a9a318f0c7e93b18f2805ba9695862
ammatinho <anamargaridamatinho@gmail.com>
2021-03-31 16:15:25 +0200

    Order Pay Reducer & Action

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -5,6 +5,9 @@ import {
   ORDER_DETAILS_REQUEST,
   ORDER_DETAILS_SUCCESS,
   ORDER_DETAILS_FAIL,
+  ORDER_PAY_REQUEST,
+  ORDER_PAY_SUCCESS,
+  ORDER_PAY_FAIL,
 } from '../constants/orderConstants'
 
 export const orderCreateReducer = (state = {}, action) => {
@@ -49,6 +52,29 @@ export const orderDetailsReducer = (
         loading: false,
         error: action.payload,
       }
+    default:
+      return state
+  }
+}
+
+export const orderPayReducer = (state = {}, action) => {
+  switch (action.type) {
+    case ORDER_PAY_REQUEST:
+      return {
+        loading: true,
+      }
+    case ORDER_PAY_SUCCESS:
+      return {
+        loading: false,
+        success: true,
+      }
+    case ORDER_PAY_FAIL:
+      return {
+        loading: false,
+        error: action.payload,
+      }
+    case ORDER_PAY_REQUEST:
+      return {}
     default:
       return state
   }
