bc15b8bafdf7f26b77b7be73fab1d18f7439e799
ammatinho <anamargaridamatinho@gmail.com>
2021-04-06 15:15:05 +0200

    Admin Screen Access Security

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -5,6 +5,7 @@ import {
   USER_DETAILS_SUCCESS,
   USER_LIST_FAIL,
   USER_LIST_REQUEST,
+  USER_LIST_RESET,
   USER_LIST_SUCCESS,
   USER_LOGIN_FAIL,
   USER_LOGIN_REQUEST,
@@ -128,6 +129,10 @@ export const userListReducer = (state = { users: [] }, action) => {
       return {
         loading: false,
         error: action.payload,
+      }
+    case USER_LIST_RESET:
+      return {
+        users: [],
       }
     default:
       return state
