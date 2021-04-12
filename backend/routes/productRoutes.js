8f31eead3be0e825f93880e1a82cc55c5822ce26
ammatinho <anamargaridamatinho@gmail.com>
2021-04-12 11:16:32 +0200

    Admin Delete Products

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,10 +3,12 @@ const router = express.Router()
 import {
   getProductById,
   getProducts,
+  deleteProduct,
 } from '../controllers/productController.js'
+import { protect, admin } from '../middleware/authMiddleware.js'
 
 router.route('/').get(getProducts)
 
-router.route('/:id').get(getProductById)
+router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct)
 
 export default router
