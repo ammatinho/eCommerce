e0c1acf76880cfdaef4b153ec7c4fd1161704b8c
ammatinho <anamargaridamatinho@gmail.com>
2021-04-13 15:48:21 +0200

    Mark Order As Delivered

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -79,6 +79,25 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
   }
 })
 
+// @description     Update order to delivered
+// @route           PUT /api/orders/:id/deliver
+// @access          Private/Admin
+const updateOrderToDelivered = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id)
+
+  if (order) {
+    order.isDelivered = true
+    order.deliveredAt = Date.now()
+
+    const updatedOrder = await order.save()
+
+    res.json(updatedOrder)
+  } else {
+    res.status(404)
+    throw new Error('Order not found')
+  }
+})
+
 // @description     Get logged in user orders
 // @route           GET /api/orders/myorders
 // @access          Private
@@ -99,6 +118,7 @@ export {
   addOrderItems,
   getOrderById,
   updateOrderToPaid,
+  updateOrderToDelivered,
   getMyOrders,
   getOrders,
 }
