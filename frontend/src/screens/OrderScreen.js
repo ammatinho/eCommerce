751e9190bf84ce63c13e407d277eef23ab7792bd
ammatinho <anamargaridamatinho@gmail.com>
2021-04-13 16:09:43 +0200

    Fix Bug & Add Missing Dependencies

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -62,7 +62,7 @@ const OrderScreen = ({ match, history }) => {
       document.body.appendChild(script)
     }
 
-    if (!order || successPay || successDeliver) {
+    if (!order || successPay || successDeliver || order._id != orderId) {
       dispatch({ type: ORDER_PAY_RESET })
       dispatch({ type: ORDER_DELIVER_RESET })
       dispatch(getOrderDetails(orderId))
@@ -73,7 +73,7 @@ const OrderScreen = ({ match, history }) => {
         setSdkReady(true)
       }
     }
-  }, [dispatch, order, orderId, successPay, successDeliver])
+  }, [dispatch, order, orderId, successPay, successDeliver, history, userInfo])
 
   const successPaymentHandler = (paymentResult) => {
     console.log(paymentResult)
