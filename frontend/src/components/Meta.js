e4847e18910e9380a5d4475453f6dd5ce56138d2
ammatinho <anamargaridamatinho@gmail.com>
2021-04-16 14:05:06 +0200

    Custom Page Titles & Meta

---
+++ frontend/src/components/Meta.js
@@ -0,0 +1,21 @@
+import { Helmet } from 'react-helmet'
+
+import React from 'react'
+
+const Meta = ({ title, description, keywords }) => {
+  return (
+    <Helmet>
+      <title>{title}</title>
+      <meta name='description' content={description} />
+      <meta name='keyword' content={keywords} />
+    </Helmet>
+  )
+}
+
+Meta.defaultProps = {
+  title: 'Welcome To eCommerce',
+  description: 'We sell the best products for cheap',
+  keywords: 'electronics, buy electronics, cheap electronics',
+}
+
+export default Meta
